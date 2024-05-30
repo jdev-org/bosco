02a745b9b9e730e8b3e400a04723350b7d88915a
Agath21 <agathe.adam@jdev.fr>
2024-05-30 15:35:32 +0200

    Update template with style and graph label #2

--- bosco/addons/stats-ogc/js/statsUtils.js
+++ bosco/addons/stats-ogc/js/statsUtils.js
@@ -13,17 +13,35 @@ export const requestStats = (params, uuidContainer, parcelleId) => {
       };
     const req = new Request(nameRequest, null, service, postParams);
     const onReq = ({response}) => {
+        // Create range area with q25 & q75
+        let dataArea = [] ;
+        let arrLength = response.quartile_25.length;
+        for(let i = 0; i < arrLength; i++){
+            dataArea.push([response.quartile_25[i],response.quartile_75[i]]);
+        }
         const series = [
-            {data: response.mediane, name: "Médiane"},
-            {data: response.mediane_annee_cible, name: "Médiane année"},
-            {data: response.quartile_25, name: "Quartile 25"},
-            {data: response.quartile_75, name: "Quartile 75"}
+            {data: response.mediane,type: 'line', name: "Normale de saison (2017-2022)",color:'#4cbbe0',zIndex: 1},
+            {data: response.mediane_annee_cible,type: 'line', name: "Année sélectionnée",color:'#204f9c',zIndex: 2,
+            marker: {
+                fillColor: 'white',
+                lineWidth: 2,
+                lineColor: '#204f9c'
+            }},
+            {data: dataArea, name: "Minimale - Maximale",type: 'arearange',lineWidth: 0,linkedTo: ':previous',color:'#4cbbe038',fillOpacity: 0.3,zIndex: 0,
+            marker: {
+                enabled: false
+            }}
         ];
 
         createChart(series, uuidContainer, {
             title: {
                 // text: `Soil Moisture climatology (${postParams.year_etude})`
-                text: "Soil Moisture climatology 2017-2022"
+                text: ""
+            },
+            yAxis: {
+                title: {
+                    text: "Teneur en eau de surface (%)"
+                }
             }
         })
     }
@@ -41,15 +59,18 @@ export const createChart = (series, container, additionalConfig) => {
             align: 'left'
         },
         chart: {
-            type: 'line'
+            type: 'line',
+            style: {
+                fontFamily: "Montserrat"
+            }
         },
         legend: {
             align: 'center',
             verticalAlign: 'top',
         },
         xAxis : {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            categories: ['Jan', 'Fév', 'Mars', 'Avril', 'Mai', 'Juin',
+            'Juil', 'Août', 'Sept', 'Oct', 'Nov', 'Déc'],
              
         },
         yAxis: {
@@ -59,11 +80,13 @@ export const createChart = (series, container, additionalConfig) => {
                 color: 'silver'
             }],
             title: {
-                text: "Surface Soil Moisture %"
+                text: "Teneur en eau de surface (%)"
             }
         },
         tooltip: {
-            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+            //pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+            //shared: true,
+            valueSuffix: '%',
             valueDecimals: 2,
             split: true
         },
